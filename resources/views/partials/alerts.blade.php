f889abbb63cd851c11907b7198a23b12869d2177
Exlosir <tna290@gmail.com>
2019-05-30 15:58:10 +0500

    Обновлена русская локализация

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -2,7 +2,7 @@
     @foreach($errors->all() as $error)
         <div class="alert alert-danger alert-dismissible fade show text-center mt-3" role="alert">
 
-            {{$error}}
+            {!!$error!!}
             {{--@{{ error.title }}--}}
 
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -16,7 +16,7 @@
 
     <div class="alert alert-success alert-dismissible fade show text-center mt-3" role="alert">
 
-        {{\Illuminate\Support\Facades\Session::get('success')}}
+        {!!\Illuminate\Support\Facades\Session::get('success')!!}
         {{--@{{ success.title }}--}}
 
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -29,10 +29,10 @@
 
     <div class="alert alert-warning alert-dismissible fade show text-center mt-3" role="alert">
 
-        {{\Illuminate\Support\Facades\Session::get('warning')}}
+    {!!\Illuminate\Support\Facades\Session::get('warning')!!}
 
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true"><i class="fas fa-times-circle"></i></span>
-        </button>
-    </div>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true"><i class="fas fa-times-circle"></i></span>
+    </button>
+</div>
 @endif
